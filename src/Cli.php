77f18e43f58e05c933b36ab3b2f7c9c5d2e778f8
lapihin.andrei <lapihin.andrei@yandex.ru>
2025-06-09 14:39:00 +0300

    Add greetings in brain-games
    Require wp-cli/php-cli-tools
    add in file src/Cli.php function greetings()

---
+++ src/Cli.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Hexlet\Code\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+function greetings()
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+}
