65d705323d497682f368d434ca61f015bb9be98f
lapihin.andrei <lapihin.andrei@yandex.ru>
2025-06-16 17:10:24 +0300

    Add Brain-Gcd game
    Game where you must calculate a great common divisor

---
+++ src/Games/BrainGcd.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Hexlet\Code\Games\BrainGcd;
+
+use function cli\line;
+use function Hexlet\Code\Cli\greetings;
+use function Hexlet\Code\Engine\engine;
+
+function run(): void
+{
+    $name = greetings();
+    $prefix = 'Hexlet\Code\Games\BrainGcd';
+    line('Find the greatest common divisor of given numbers.');
+    engine($name, $prefix);
+}
+
+function getQuestion()
+{
+    $firstOperand = mt_rand(1, 40);
+    $secondOperand = mt_rand(1, 40);    
+    line("Question: {$firstOperand} {$secondOperand}");
+    return strval(findGcd($firstOperand, $secondOperand));
+}
+
+function getRightAnswer($randValue)
+{
+    return $randValue;
+}
+
+function findGcd($first, $second)
+{   $max = max($first, $second);
+    $min = min($first, $second);     
+    while ($min !== 0) {        
+        $remainder = $max % $min;
+        $max = $min;
+        $min = $remainder;
+    }
+    return $max;
+}
