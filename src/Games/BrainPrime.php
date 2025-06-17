20ba76be330a6fc06f2c4685c15722f454ab1895
lapihin.andrei <lapihin.andrei@yandex.ru>
2025-06-17 20:26:03 +0300

    Add Brain-Prime game
    Add game where you must determine: is number a prime?

---
+++ src/Games/BrainPrime.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Hexlet\Code\Games\BrainPrime;
+
+use function cli\line;
+use function Hexlet\Code\Cli\greetings;
+use function Hexlet\Code\Engine\engine;
+
+function run(): void
+{
+    $name = greetings();
+    $prefix = 'Hexlet\Code\Games\BrainPrime';
+    line('Answer "yes" if given number is prime. Otherwise answer "no".');
+    engine($name, $prefix);
+}
+
+function getQuestion()
+{
+    $randValue = mt_rand(0, 100);
+    line("Question: {$randValue}");
+    return strval(getRightAnswer($randValue));
+}
+
+function getRightAnswer($randValue)
+{
+    return isPrime($randValue) ? "yes" : "no";
+}
+
+function isPrime($value)
+{
+    if ($value === 0) {
+        return false;
+    }
+    for ($i = 2; $i < $value; $i++) {
+        if ($value % $i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
